9b6e839f26789a6ef29f2be2a89504c0059aa74a
ahmedhdeawy <ahmadhdeawy@namaait.com>
2022-01-17 07:20:19 +0200

    Configure Test Cases

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'  =>  'v1', 'namespace'   =>  'App\Http\Controllers\Api\V1'], function () {
-    Route::get('phone-numbers', 'ApiController@phoneNumbersList')->name('phone-numbers.list');
+    Route::get('phone-numbers', 'ApiController@phoneNumbersList')->name('api.phone-numbers.list');
 });
