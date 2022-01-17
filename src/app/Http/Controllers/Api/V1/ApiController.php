6813aad3b6f0c273b16cce26f3489fa71415ab8f
ahmedhdeawy <ahmadhdeawy@namaait.com>
2022-01-17 07:10:57 +0200

    Create Manual Pagination

--- src/app/Http/Controllers/Api/V1/ApiController.php
+++ src/app/Http/Controllers/Api/V1/ApiController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Routing\Controller;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\CustomerResource;
 use App\Repositories\CustomerRepositiory;
 
--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param mixed $items
+     * @param array $options
+     * 
+     * @return LengthAwarePaginator
+     */
+    public function paginate($items, $options = [])
+    {
+        $page = request('page') ?: (Paginator::resolveCurrentPage() ?: 1);
+        $perPage = request('perPage') ?? 15;
+
+        $paginator = new LengthAwarePaginator(
+            $items->forPage($page, $perPage),
+            $items->count(),
+            $perPage,
+            $page,
+            $options
+        );
+
+        $paginator->setPath(env('APP_URL'));
+
+        return $paginator;
+    }
 }
